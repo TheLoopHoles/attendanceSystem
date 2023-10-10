aaaaa70a4ebddd0bd9e2f83cb96d99e44a62b9b1
Niket Verma <122077716+TheLoopHoles@users.noreply.github.com>
2023-10-10 08:55:57 +0530

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Welcome, Student</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="logo.png" alt="Portal Logo">
+            </div>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Attendance</a></li>
+                <li><a href="#">Reports</a></li>
+                <li><a href="#">Profile</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="student-dashboard">
+        <h2>Welcome, Student!</h2>
+        <p>Here, you can access your attendance records, view reports, and manage your profile.</p>
+        <div class="actions">
+            <a href="#">View Attendance</a>
+            <a href="#">View Reports</a>
+            <a href="#">Edit Profile</a>
+            <a href="logout.php">Logout</a>
+        </div>
+    </section>
+</body>
+</html>
